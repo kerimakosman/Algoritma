a9aa8ae23cb655bb6181558cb0719bfaf4c60834
Kerim Akosman <kerimakosman@hotmail.com>
2022-10-27 15:22:58 +0300

    3-5 in katı KerimAkosman yazdırma

---
+++ Oruntu_1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Oruntu_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //3ve5in katı=KerimAkosman, 3ün katı Kerim, 5in katı=Akosman, harici i nin kendisi
+
+            Console.WriteLine("Lütfen Bir Sayi Giriniz :");
+            int girilensayi = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= girilensayi; i++)
+            {
+                if (i % 15 == 0)
+                    Console.WriteLine("KerimAkosman");
+                else if (i % 3 == 0)
+                    Console.WriteLine("Kerim");
+                else if (i % 5 == 0)
+                    Console.WriteLine("Akosman");
+                else
+                    Console.WriteLine(i);
+            }
+
+        }
+    }
+}
